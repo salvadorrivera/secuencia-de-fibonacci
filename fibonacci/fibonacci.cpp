811a43399cbd4a34b1743b87870642f9618e1728
salvadorrivera <salvadorivera2004@gmail.com>
2021-09-28 09:55:10 -0300

    Agregar archivos de proyecto.

---
+++ fibonacci/fibonacci.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n1 = 0, n2 = 1, n3, i, number;
+
+	cout << "introduzca un largo para la secuencia: ";
+	cin >> number;
+	cout << n1 << " " << n2 << " "; // imprime 0 y 1
+	for (i = 2; i < number; i++)
+	{
+		n3 = n1 + n2;
+		cout << n3 << " ";
+		n1 = n2;
+		n2 = n3;
+	}
+	return 0;
+}
